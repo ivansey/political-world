65396956d1cf0fb520537d3d63b0665e54607639
PythonerMMM <40696631+PythonerMMM@users.noreply.github.com>
2018-11-01 19:19:57 +0500

    Update chat_engine.php
    Оптимизировано

--- chat_engine.php
+++ chat_engine.php
@@ -4,14 +4,10 @@ auth();
 banned($user);
 banned_chat($user);
 $noheader;
-//Проверка количества сообщений
-$sql = $conn->query("SELECT COUNT(*) FROM `chat`")->fetch()['COUNT(*)'];
-//Вывод сообщений
-$i = 0;
-while ($i < $sql) {
-    $i++;
-    $echo = $conn->query("SELECT * FROM `chat` LIMIT " . $i . "," . $i . " ")->fetch();
-    $msg = $echo['name'] . " [" . $echo['time'] . "] " . " : " . $echo['text'] . "<br>";
+
+$messages = $conn->query("SELECT * FROM `chat`");
+while($message=$messages->fetch()){
+$msg = $message['name'] . " [" . $message['time'] . "] " . " : " . $message['text'] . "<br>";
     echo($msg);
 }
 ?>
