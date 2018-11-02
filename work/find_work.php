dcfcb25f04db94b578fa194d4c8911145b5cbc27
PythonerMMM <40696631+pythonermmm@users.noreply.github.com>
2018-11-02 19:23:18 +0300

    Optimized Find Work

--- work/find_work.php
+++ work/find_work.php
@@ -7,17 +7,12 @@ $sql = $conn->query("SELECT COUNT(*) FROM `factory`")->fetch()['COUNT(*)'];
 $i = 0;
 
 echo '<div class="block">Работа<br>';
-$sum = $conn->query("SELECT COUNT(*) FROM `factory`")->fetch()['COUNT(*)'];
-$i = 0;
-$i2 = 1;
-while($sum >= $i) {
-	$sql = $conn->query("SELECT * FROM factory LIMIT " . $i . "," . $i2 . "")->fetch();
-	echo '<div class="block">Имя: ' . $sql['name'] . ', тип: ' . $sql['type'] . ', зарплата: ' . $sql['salary'] . ', процент выдаваемого ресурса: ' . $sql['res'];
+$sqle = $conn->query("SELECT * FROM `factory`");
+while($sql=$sqle->fetch()){
+echo '<div class="block">Имя: ' . $sql['name'] . ', тип: ' . $sql['type'] . ', зарплата: ' . $sql['salary'] . ', процент выдаваемого ресурса: ' . $sql['res'];
 	echo '
 		<div class="a"><a href="factory_viev.php?factory_id=' . $sql['id'] . '">Открыть страницу фабрики</a></div></div><br>
 	';
-	$i++;
-	$i2++;
 }
 echo '</div>';
 echo '<div class="a"><a href="index.php">Назад</a></div>';
