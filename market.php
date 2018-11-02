b9556c251842c031a2457414e7113ee9e527fa6d
PythonerMMM <40696631+pythonermmm@users.noreply.github.com>
2018-11-02 19:46:13 +0300

    Optimized Market

--- market.php
+++ market.php
@@ -22,42 +22,5 @@ echo '<div class="block">ID: ' . $market['id_user'] . ', тип: ' . $market['ty
 	';
 }
 }
-/*if (isset($_POST['sort_type'])) {
-    $store = $conn->query("SELECT * FROM `store` WHERE `id` = " . $user['id'] . " ")->fetch();
-    $sum = $conn->query("SELECT COUNT(*) FROM `market`")->fetch()['COUNT(*)'];
-    $i = 0;
-    $i2 = 1;
-    while ($i = $sum) {
-        $i++;
-        $i2++;
-        $market = $conn->query("SELECT * FROM `market` ORDER BY `price` LIMIT " . $i . ",1 ")->fetch();
-        echo '<div class="block">ID: ' . $market['id_user'] . ', тип: ' . $market['type'] . ', цена:' . $market['price'] . ', кол-во:' . $market['res'];
-        echo '
-		<form action="buy_market.php?id=' . $market['id'] . '" method="post">
-			<div class="a"> <input type="text" name="res"> 
-			<input type="submit" name="buy" value="Купить"></div>
-		</form>
-		</div>
-	';
-    }
-}*/
-/*$store = $conn->query("SELECT * FROM `store` WHERE `id` = " . $user['id'] . " ")->fetch();
-$sum = $conn->query("SELECT COUNT(*) FROM `market`")->fetch()['COUNT(*)'];
-$i = 0;
-$i2 = 1;
-while ($i <= $sum) {
-    $i++;
-    $i2++;
-    $market = $conn->query("SELECT * FROM `market` LIMIT " . $i . "," . $i2 . " ")->fetch();
-    echo 'ID: ' . $market['id_user'] . ', тип: ' . $market['type'] . ', цена:' . $market['price'] . ', кол-во:' . $market['res'];
-    echo '
-    <div class="block">
-		<form action="buy_market.php?id=' . $market['id'] . '" method="post">
-			<input type="text" name="res"> 
-			<input type="submit" name="buy" value="Купить">
-		</form></div>
-	';
-
-}*/
 echo '<div class="a"><a href="select_market.php">На главную</a></div>';
 ?>
