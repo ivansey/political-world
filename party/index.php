3a76d031156ba07250748e3187c49ba6aa51cc6c
ivansey <van.secret2015@gmail.com>
2018-11-02 17:58:31 +0300

    Add Search Party

--- party/index.php
+++ party/index.php
@@ -2,9 +2,20 @@
 include('../system/func.php');
 banned($user);
 auth();
-
-
-$partys = $conn->query("SELECT * FROM `party`");
+echo '
+    <div class="block">
+        <form action="" method="post">
+            Поиск партии<br>
+            <input type="text" name="party"><br>
+            <div class="a"><input type="submit" name="search" value="Поиск"></div>
+        </form>
+    </div><br>
+';
+if (isset($_POST['search'])) {
+    $partys = $conn->query("SELECT * FROM `party` WHERE `name` LIKE '%" . $_POST['party'] . "%' ");
+}else{
+    $partys = $conn->query("SELECT * FROM `party`");
+}
 echo '<div class="block"> Список партий:<br>';
 while ($party = $partys->fetch()) {
     echo '<div class="a"><a href=party_view.php?id=' . $party['id'] . '>' . $party['name'] . '</a></div><br>';
