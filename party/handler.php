eb1ac3561251a35f789e67d605f11be462d0b5a6
ivansey <van.secret2015@gmail.com>
2018-11-02 18:07:58 +0300

    XSS-Protected Create Party

--- party/handler.php
+++ party/handler.php
@@ -8,10 +8,10 @@ $tag = $_POST['tag'];
 try {
     if ($user['gold'] >= 100) {
         $query = $conn->prepare('INSERT INTO `party` SET `name` = :name, `about` = :about, `leader` = :leader, `tag` = :tag');
-        $query->bindValue(":name", $_POST['name']);
-        $query->bindValue(":about", $_POST['about']);
+        $query->bindValue(":name", htmlentities($_POST['name']));
+        $query->bindValue(":about", htmlentities($_POST['about']));
         $query->bindValue(":leader", $user['id']);
-        $query->bindValue(":tag", $tag);
+        $query->bindValue(":tag", htmlentities($tag));
         $query->execute();
         $party = $conn->query("SELECT * FROM `party` WHERE `leader` = " . $user['id'] . " ")->fetch();
         $conn->query("UPDATE `users` SET `party` = " . $party['id'] . ", `gold` = `gold` - 100, `tag` = '" . $tag . "' WHERE `id` = " . $user['id'] . " ");
