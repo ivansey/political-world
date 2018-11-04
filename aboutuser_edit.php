52edd68cab0509c029a80d878b7c2fc43a6bb6c9
ivansey <van.secret2015@gmail.com>
2018-11-04 13:57:41 +0300

    SQL-Protected

--- aboutuser_edit.php
+++ aboutuser_edit.php
@@ -2,8 +2,11 @@
 include('system/func.php');
 auth();
 banned($user);
-$email = $_COOKIE['email'];
-$about = $_POST['about'];
-$conn->query("UPDATE `users` SET `about` = '" . $about . "' WHERE `mail` = '" . $email . "'");
+$id = $user['id'];
+
+$query = $conn->prepare('UPDATE `users` SET `about` = :about WHERE `id` = :id');
+$query->bindValue(":about", htmlentities($_POST['about']));
+$query->bindValue(":id", $id);
+$query->execute();
 echo("<div class='block'> Информация О себе изменена. <br> <div class='a'> <a href=profile_edit.php>Вернуться</a></div></div>");
 
