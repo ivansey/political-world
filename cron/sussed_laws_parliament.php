c968856a76784b2c1f13724dea7bcca654af28fa
ivansey <van.secret2015@gmail.com>
2018-12-06 05:57:29 +0300

    Fix Cron Susses Laws Parliament

--- cron/sussed_laws_parliament.php
+++ cron/sussed_laws_parliament.php
@@ -23,16 +23,15 @@ $i = 0;
 while ($i < $gover_sql_count) {
     $gover_sql = $conn->query("select * from goverment limit " . $i . ",1")->fetch();
     $law_sql_count = $conn->query("select count(*) from elections_law where agree > disagree and (gover_id = " . $gover_sql['id'] . ") ")->fetch()['count(*)'];
+    //$law_sql_prew = $conn->query("select * from elections_law where agree > disagree and (gover_id = " . $gover_sql['id'] . ") ")->fetch();
     $i2 = 0;
-    if ($law_sql_count['time'] <= $sql_time) {
-        while ($i2 < $law_sql_count) {
-            $law_sql = $conn->query("select * from elections_law where agree > disagree and (gover_id = " . $gover_sql['id'] . ") limit " . $i2 . ",1")->fetch();
-            echo 'test';
-            system\laws::sussed_laws($law_sql['id']);
-            system\laws::delete_elections_law($law_sql['id']);
-            echo $i2;
-            $i2++;
-        }
+    while ($i2 < $law_sql_count) {
+        $law_sql = $conn->query("select * from elections_law where agree > disagree and (gover_id = " . $gover_sql['id'] . ") limit " . $i2 . ",1")->fetch();
+        echo 'test';
+        system\laws::sussed_laws($law_sql['id']);
+        system\laws::delete_elections_law($law_sql['id']);
+        echo $i2;
+        $i2++;
     }
     $i++;
 }
